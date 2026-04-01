0b9a32808cda01b93a28b93217d3af002f4f38a7
Axsion <ignatevm601@gmail.com>
2026-04-01 13:23:44 +0700

    update №9

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import runEngine from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Предикат для проверки на простоту
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getRoundData = () => {
+  const number = getRandomInt(1, 100);
+  const question = String(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, getRoundData);
